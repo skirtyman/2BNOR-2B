dbd7a652fe95109e806b688f995f05a7526f7c55
skirtyman <76261952+skirtyman@users.noreply.github.com>
2024-03-12 09:11:55 +0000

    Update diagram.cs

--- 2BNOR!2B/diagram.cs
+++ 2BNOR!2B/diagram.cs
@@ -1046,18 +1046,24 @@ namespace _2BNOR_2B
 
         }
 
+        private string[] TrimBrackets(string[] headers)
+        {
+            for(int i = 0; i < headers.Length; i++)
+            {
+                headers[i] = headers[i].Substring(1, headers[i].Length - 2);
+            }
+            return headers;
+        } 
+
         //Links class to UI, used to draw the truth tables to the canvas. 
         public void DrawTruthTable(Canvas c, string inputExpression, bool isSteps)
         {
-            //MessageBox.Show(getNumberOfInputs("(A.B)^(A+A)", true).ToString());
-            //MessageBox.Show(getNumberOfInputs("(A.B)^(A+A)", false).ToString());
-
-
             //Removing any previously drawn tables from the canvas. 
             c.Children.Clear();
             //headers = generateTruthTableHeadersWithSteps(inputExpression);
             headers = GetHeaders(inputExpression, true);
             outputMap = GenerateOutputMap(inputExpression, headers, true);
+            headers = TrimBrackets(headers); 
             if (isSteps)
             {
                 //drawTruthTable(c, headers, outputMap);
